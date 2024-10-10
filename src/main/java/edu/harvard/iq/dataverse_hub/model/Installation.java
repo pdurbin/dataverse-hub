34bd21612483091d98ca828c99abd5b07ebc7a0e
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-10-10 11:28:33 -0400

    Added flyway dependency

--- src/main/java/edu/harvard/iq/dataverse_hub/model/Installation.java
+++ src/main/java/edu/harvard/iq/dataverse_hub/model/Installation.java
@@ -1,5 +1,7 @@
 package edu.harvard.iq.dataverse_hub.model;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,10 +12,19 @@ public class Installation {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
+    private Long installation_id;
     private String name;
+    private String description;
+    private String latitude;
+    private String longitude;
+    private String hostname;
+    private String country;
+    private String continent;
+    private Date launchYear;
+    private Boolean gdccMmember;
+    private String doiAuthority;
+    private String contact_email;
 
-    public Installation() {
-    }
+   
 
 }
